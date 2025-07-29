d3cf56d7316ccd1a98c32f4d1697a1d46b1c265f
Irina Kuznetsova <dyachenkoia@yandex.ru>
2025-07-29 14:54:00 +0300

    выполнено ТЗ спринта 15

--- src/main/java/ru/practicum/shareit/exceptions/NotAvailableException.java
+++ src/main/java/ru/practicum/shareit/exceptions/NotAvailableException.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 @Getter
 public class NotAvailableException extends RuntimeException {
     private final String fieldNameWithError;
+
     public NotAvailableException(String fieldNameWithError, String message) {
         super(message);
         this.fieldNameWithError = fieldNameWithError;
